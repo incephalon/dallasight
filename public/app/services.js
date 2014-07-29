cd0d04ec3103828feec683de1167f8ec571cba78
robert <incephalon@gmail.com>
2014-07-29 10:02:12 -0500

    added courts, jails, tours

--- public/app/services.js
+++ public/app/services.js
@@ -19,7 +19,7 @@ app.factory('weatherData', function($http, $q){
           
         
         getWeather :function () {
-                return $http.get("http://api.wunderground.com/api/1d9ddd1bf45d8b0a/forecast10day/geolookup/conditions/q/TX/Dallas.json");
+                return $http.jsonp("http://api.wunderground.com/api/1d9ddd1bf45d8b0a/forecast10day/geolookup/conditions/q/TX/Dallas.json");
             }
     
     
