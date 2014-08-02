7d0790878a26bc6aade2ab9a02aa86665955f345
Elad Shaham <elad.shaham@gmail.com>
2014-08-03 01:33:48 +0300

    finally show map

--- packages/system/public/controllers/index.js
+++ packages/system/public/controllers/index.js
@@ -5,13 +5,18 @@ angular.module('mean.system').controller('IndexController', ['$scope', 'Global',
         $scope.global = Global;
 
         $scope.map = {
-            center: new google.maps.LatLng(32.795903, -96.805301),
+            center: {
+                latitude: 32.795903,
+                longitude: -96.805301
+            },
             zoom: 14,
-            //mapTypeId: google.maps.MapTypeId.SATELLITE,
-            mapTypeId: google.maps.MapTypeId.HYBRID,
-            disableDefaultUI: true
-            //heading: 90,
-            //tilt: 45
+            options: {
+                //mapTypeId: google.maps.MapTypeId.SATELLITE,
+                mapTypeId: google.maps.MapTypeId.HYBRID,
+                disableDefaultUI: true
+                //heading: 90,
+                //tilt: 45
+            }
         };
     }
 ]);
