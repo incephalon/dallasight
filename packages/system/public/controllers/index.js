a0e69d7a251045866fa88b58b51a234546beeee1
Elad Shaham <elad.shaham@gmail.com>
2014-08-03 01:09:26 +0300

    load google maps assets properly

--- packages/system/public/controllers/index.js
+++ packages/system/public/controllers/index.js
@@ -1,7 +1,17 @@
 'use strict';
 
 angular.module('mean.system').controller('IndexController', ['$scope', 'Global',
-  function($scope, Global) {
-    $scope.global = Global;
-  }
+    function ($scope, Global) {
+        $scope.global = Global;
+
+        $scope.map = {
+            center: new google.maps.LatLng(32.795903, -96.805301),
+            zoom: 14,
+            //mapTypeId: google.maps.MapTypeId.SATELLITE,
+            mapTypeId: google.maps.MapTypeId.HYBRID,
+            disableDefaultUI: true
+            //heading: 90,
+            //tilt: 45
+        };
+    }
 ]);
